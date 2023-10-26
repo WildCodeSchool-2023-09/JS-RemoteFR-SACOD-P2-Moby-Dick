ffb4b3bdbc41506db6a786f93f0300e7405c5eb1
PinpinLaToupie <femenias.c@gmail.com>
2023-10-26 10:33:22 +0200

    video accueil

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,15 +1,11 @@
 function Home() {
   return (
     <div className="home-container">
-      <video autoPlay muted loop className="background-video" preload="auto">
-        <source src="frontend/media/pokemon wild.mp4" type="video/mp4" />
+      <video autoPlay muted loop className="background-video">
+        <source src="./accueil.mp4" type="video/mp4" />
       </video>
-      <div className="content">
-        <h1>Bienvenue à Pokemon Wild</h1>
-        <p>Appuyez sur Jouer pour commencer l'aventure</p>
-        <button type="button" className="play-button">
-          Jouer
-        </button>
+      <div className="play-button">
+        <div className="play-icon">▷</div>
       </div>
     </div>
   );
