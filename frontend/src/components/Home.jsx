5f2d141e3395f367b4f43f3cd863fb2b07afd632
PinpinLaToupie <femenias.c@gmail.com>
2023-10-26 12:06:07 +0200

    logo play pokeball + button music

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
+
 function Home() {
+  const [isMusicPlaying, setIsMusicPlaying] = useState(true);
+
+  const toggleMusic = () => {
+    setIsMusicPlaying(!isMusicPlaying);
+  };
   return (
     <div className="home-container">
       <video autoPlay muted loop className="background-video">
         <source src="./accueil.mp4" type="video/mp4" />
       </video>
       <div className="play-button">
-        <div className="play-icon">▷</div>
+        <img src="./pokeball.png" alt="play" />
       </div>
+      <button type="button" onClick={toggleMusic} className="music-toggle">
+        {isMusicPlaying ? "🔕" : "🎵"}
+      </button>
+      <audio autoPlay loop>
+        {isMusicPlaying && <source src="./son-accueil.mp3" type="audio/mpeg" />}
+        <track kind="captions" label="French" srcLang="fr" src="" default />
+      </audio>
     </div>
   );
 }
