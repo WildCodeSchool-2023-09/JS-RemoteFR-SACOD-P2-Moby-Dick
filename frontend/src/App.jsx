394f8e9092cd42438ca869ca0fc8d5157012b4c9
PinpinLaToupie <femenias.c@gmail.com>
2023-10-26 15:57:41 +0200

    componenets hub

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Home from "./components/Home";
+import Hub from "./components/Hub";
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <Hub />
     </div>
   );
 }
---
+++ frontend/src/components/Hub.jsx
@@ -0,0 +1,23 @@
+function Hub() {
+  return (
+    <div className="hub-container">
+      <div className="menu">
+        <div className="logo">
+          <img src="/pokeball.png" alt="Pokeball Logo" />
+        </div>
+        <ul className="menu-list">
+          <li>Pokemon 1</li>
+          <li>Pokemon 2</li>
+          <li>Pokemon 3</li>
+          <li>Pokemon 4</li>
+          <li>Pokemon 5</li>
+          <li>Pokemon 6</li>
+        </ul>
+      </div>
+      <div className="body">
+        <img src="./town.png" alt="Hub" />
+      </div>
+    </div>
+  );
+}
+export default Hub;
