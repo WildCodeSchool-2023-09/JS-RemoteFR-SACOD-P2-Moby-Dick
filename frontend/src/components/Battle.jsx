b863ded2263cb95b4581ba50cb19ac6254ada609
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-10-30 11:43:24 +0100

    random background and trainer system

--- frontend/src/components/Battle.jsx
+++ frontend/src/components/Battle.jsx
@@ -1,29 +1,56 @@
 import React, { useState, useEffect } from "react";
 
 const images = [
-  "background1.png",
-  "background2.png",
-  "background3.png",
-  "background4.png",
-  "background5.png",
-  "background6.png",
-  "background7.png",
-  "background8.png",
-  "background9.png",
+  "backgroundbeach.png",
+  "backgroundearth.png",
+  "backgroundforest.png",
+  "backgroundsea.png",
+  "backgroundgrass.png",
+];
+const trainers = [
+  "dresseur1.png",
+  "dresseur2.png",
+  "dresseur3.png",
+  "dresseur4.png",
+  "dresseur5.png",
+  "dresseur6.png",
+  "dresseur7.png",
+  "dresseur8.png",
+  "dresseur9.png",
+  "dresseur10.png",
+  "dresseur11.png",
+  "dresseur12.png",
+  "dresseur13.png",
+  "dresseur14.png",
+  "dresseur15.png",
+  "dresseur16.png",
+  "dresseur17.png",
+  "dresseur18.png",
+  "dresseur19.png",
+  "dresseur21.png",
+  "dresseur22.png",
+  "dresseur23.png",
+  "dresseur24.png",
 ];
 
 function Battle() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [selectedTrainer, setSelectedTrainer] = useState(null);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * images.length);
+    const randomTrainer = Math.floor(Math.random() * trainers.length);
     setSelectedImage(images[randomIndex]);
+    setSelectedTrainer(trainers[randomTrainer]);
   }, []);
 
   return (
     <div className="Battle">
       {selectedImage && (
         <img className="battleGround" src={selectedImage} alt="Battleground" />
+      )}
+      {selectedTrainer && (
+        <img className="trainers" src={selectedTrainer} alt="Trainers" />
       )}
     </div>
   );
