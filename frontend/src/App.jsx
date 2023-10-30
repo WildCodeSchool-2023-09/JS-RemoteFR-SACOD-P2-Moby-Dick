d135a89f7b70d50026c1e4db02a1c1d09fdf41df
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-10-30 15:45:18 +0100

    battleground wild

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Battle from "./components/Battle";
+import BattleWild from "./components/BattleWild";
 
 function App() {
   return (
     <div>
       <NavBar />
+      <BattleWild />
       <Battle />
     </div>
   );
