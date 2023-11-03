0158fbbcbe6754c84e3e9a591c9b6467fbb05f3b
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-11-03 13:15:21 +0100

    rooter with new react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Routes, Route, useMatch } from "react-router-dom";
 import NavBar from "./components/NavBar";
 import Battle from "./components/Battle";
 import BattleWild from "./components/BattleWild";
@@ -7,34 +6,17 @@ import Home from "./components/Home";
 import Hub from "./components/Hub";
 import PokeCenter from "./components/PokeCenter";
 import Footer from "./components/Footer";
-import MenuPlayer from "./components/MenuPlayer";
 import "./App.css";
 
 function App() {
-  const isHub = useMatch("/hub");
-  const isBattle = useMatch("/battle");
-  const isBattleWild = useMatch("/battle-wild");
-  const isPokeCenter = useMatch("/poke-center");
-
   return (
     <div>
       <NavBar />
-      <div className="container">
-        {(isHub || isBattle || isBattleWild || isPokeCenter) && (
-          <div className="menuPlayerContainer">
-            <MenuPlayer />
-          </div>
-        )}
-        <main className="battleContainer">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="hub" element={<Hub />} />
-            <Route path="battle" element={<Battle />} />
-            <Route path="battle-wild" element={<BattleWild />} />
-            <Route path="poke-center" element={<PokeCenter />} />
-          </Routes>
-        </main>
-      </div>
+      <Home />
+      <Hub />
+      <BattleWild />
+      <Battle />
+      <PokeCenter />
       <Footer />
     </div>
   );
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,76 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./components/Home";
+import Hub from "./components/Hub";
+import NavBar from "./components/NavBar";
+import Battle from "./components/Battle";
+import Footer from "./components/Footer";
+import MenuPlayer from "./components/MenuPlayer";
+import BattleWild from "./components/BattleWild";
+import PokeCenter from "./components/PokeCenter";
+import "./App.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Home />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/Hub",
+    element: (
+      <>
+        <NavBar />
+        <Hub />
+        <MenuPlayer />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/Battle",
+    element: (
+      <>
+        <NavBar />
+        <MenuPlayer />
+        <Battle />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/BattleWild",
+    element: (
+      <>
+        <NavBar />
+        <MenuPlayer />
+        <BattleWild />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/BattleWild",
+    element: (
+      <>
+        <NavBar />
+        <MenuPlayer />
+        <PokeCenter />
+        <Footer />
+      </>
+    ),
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
