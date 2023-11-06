dcd7ad083daeea991c194c4b3b81264007689194
Jonathan-Bernard <johnnyboy59000@gmail.com>
2023-11-06 17:17:57 +0100

    Add API and random pokemonWild

--- frontend/src/components/BattleWild.jsx
+++ frontend/src/components/BattleWild.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import api from "../api";
 
 const images = [
   "backgroundbeach.png",
@@ -10,16 +11,37 @@ const images = [
 
 function BattleWild() {
   const [selectedImage, setSelectedImage] = useState(null);
+  const [pokemonImage, setPokemonImage] = useState(null);
 
   useEffect(() => {
     const randomIndex = Math.floor(Math.random() * images.length);
     setSelectedImage(images[randomIndex]);
+
+    const fetchPokemonData = async () => {
+      try {
+        const maxPokemon = 151;
+        const randomId = Math.floor(Math.random() * maxPokemon) + 1;
+        const response = await api.get(`/${randomId}`);
+        const pokemonImageUrl = response.data.sprites.front_default;
+        setPokemonImage(pokemonImageUrl);
+      } catch (error) {
+        console.error(
+          "Erreur lors de la récupération des données du Pokémon",
+          error
+        );
+      }
+    };
+
+    fetchPokemonData();
   }, []);
 
   return (
     <div className="Battle">
       {selectedImage && (
         <img className="battleGround" src={selectedImage} alt="Battleground" />
+      )}
+      {pokemonImage && (
+        <img className="pokemonWild" src={pokemonImage} alt="Pokemon" />
       )}
     </div>
   );
