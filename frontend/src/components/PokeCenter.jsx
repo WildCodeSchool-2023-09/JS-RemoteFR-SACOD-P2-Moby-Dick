fd149eb55ecae78d23258d61d793aa85ee83b299
Jordan MIEGER <miegerjordan2000@gmail.com>
2023-11-15 16:59:29 +0100

    Ajout fonction heal PokeCenter

--- frontend/src/components/PokeCenter.jsx
+++ frontend/src/components/PokeCenter.jsx
@@ -1,8 +1,19 @@
+import { useContext } from "react";
+import { PokemonContext } from "./PokemonContext";
+
 function PokeCenter() {
+  const { team, setPokemonHp } = useContext(PokemonContext);
+
+  const handleHeal = () => {
+    team.forEach((pokemon) => {
+      setPokemonHp(pokemon.name, pokemon.hp);
+    });
+  };
+
   return (
     <div className="PokeCenter">
-      <p className="textPokeCenter">Bonjour, tu veux soigner tes Pokemon ?</p>
-      <button className="healPokemonButton" type="button">
+      <p className="textPokeCenter">Bonjour, tu veux soigner tes Pokémons ?</p>
+      <button onClick={handleHeal} className="healPokemonButton" type="button">
         Oui infirmière Joëlle !
       </button>
     </div>
